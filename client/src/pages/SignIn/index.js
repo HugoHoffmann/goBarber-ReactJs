b142f048f422536f4bea04f7f9aa34f2ab626d6d
Hugo <hugohoffmann04@gmail.com>
2019-09-20 17:52:45 -0300

    .

--- client/src/pages/SignIn/index.js
+++ client/src/pages/SignIn/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom'
+// import logo from '~/assets/logo.png';
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
-    <div>Login</div>
+    <>
+      <img src="" alt="GoBarber"/>
+      <form>
+        <input type="email" placeholder="Seu E-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">
+          Acessar
+        </button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
   );
 }
--- client/src/pages/_layouts/auth/styles.js
+++ client/src/pages/_layouts/auth/styles.js
@@ -3,4 +3,35 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
     height: 100%;
     background: linear-gradient(-90deg, #7159c1, #ab59c1);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 315px;
+    text-align: center;
+
+    form{
+        display: flex;
+        flex-direction: column;
+        margin-top: 30px;
+
+        input{
+            background: rgba(0, 0, 0, 0.1);
+            border: 0;
+            border-radius: 4px;
+            height: 44px;
+            padding: 0 15px;
+            color: #fff;
+            margin: 0 0 10px;
+
+            &::placeholder{
+                color: rgba(255, 255, 255, 0.7);
+            }
+        }
+    }
 `;
