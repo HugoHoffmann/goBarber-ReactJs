c5474e86d2e08d56240902175f4a2e5c46e623ca
Hugo <hugohoffmann04@gmail.com>
2019-09-20 11:44:44 -0300

    chore: config reactotron

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -5,13 +5,13 @@ import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 
 import Dashboard from '../pages/Dashboard';
-import Profile from '../pagesProfile';
+import Profile from '../pages/Profile';
 
 export default function Routes(){
     return (
         <Switch>
             <Route path="/" exact component={SignIn} />
-            <Route path="/register" exact component={SignIn} />
+            <Route path="/register" exact component={SignUp} />
             <Route path="/dashboard" exact component={Dashboard} />
             <Route path="/profile" exact component={Profile} />
         </Switch>
