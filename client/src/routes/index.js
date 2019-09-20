f53baec670f9b9a63b69dd403a8c423ad2df30a7
Hugo <hugohoffmann04@gmail.com>
2019-09-20 14:40:33 -0300

    new: private routes

--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -12,8 +12,9 @@ export default function Routes(){
         <Switch>
             <Route path="/" exact component={SignIn} />
             <Route path="/register" exact component={SignUp} />
-            <Route path="/dashboard" exact component={Dashboard} />
-            <Route path="/profile" exact component={Profile} />
+            
+            <Route path="/dashboard" exact component={Dashboard} isPrivate />
+            <Route path="/profile" exact component={Profile} isPrivate />
         </Switch>
     );
 
