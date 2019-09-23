f3ad7120eff28e658b09d18fcb34bb9247241389
Hugo <hugohoffmann04@gmail.com>
2019-09-23 09:39:30 -0300

    chore: config store - redux|redux-saga

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {Router} from 'react-router-dom';
-
+import { Provider } from 'react-redux'
 import Routes from './routes';
 import history from './services/history';
 import './config/ReactotronConfig';
 import GlobalStyle from './styles/global';
-
+import store from './store';
 function App() {
   return (
-    <Router history={history}>
-      <Routes/> 
-      <GlobalStyle/>
-    </Router>
+    <Provider store={store} >
+      <Router history={history}>
+        <Routes/> 
+        <GlobalStyle/>
+      </Router>
+    </Provider>
   );
 }
 
