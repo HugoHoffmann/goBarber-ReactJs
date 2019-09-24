cf917bf36136666dacee59c407d15a34b86b76d7
Hugo <hugohoffmann04@gmail.com>
2019-09-24 11:51:30 -0300

    chore: show header datas

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,32 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import logo from '~/assets/img/logo2.svg';
 import Notifications from '~/components/Notifications';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
-  return (
-    <Container>
-        <Content>
-            <nav>
-                <img src={logo} alt="Logo roxa"/>
-                <Link to="/dashboard">DASHBOARD</Link>
-            </nav>
-            <aside>
-                <Notifications/>
-                <Profile>
-                    <div>
-                        <strong>
-                            Hugo Hoffmann
-                        </strong>
-                        <Link to="/profile">
-                            Meu perfil
-                        </Link>
-                    </div>
-                    <img src="https://api.adorable.io/avatars/50/abott@adorable.png" alt="" />
-                </Profile>
-            </aside>
-        </Content>
-    </Container>
-  );
+    const profile = useSelector(state => state.user.profile);
+    return (
+        <Container>
+            <Content>
+                <nav>
+                    <img src={logo} alt="Logo roxa"/>
+                    <Link to="/dashboard">DASHBOARD</Link>
+                </nav>
+                <aside>
+                    <Notifications/>
+                    <Profile>
+                        <div>
+                            <strong>
+                                {profile.name}
+                            </strong>
+                            <Link to="/profile">
+                                Meu perfil
+                            </Link>
+                        </div>
+                        <img src={profile.avatar.url || 'https://api.adorable.io/avatars/50/abott@adorable.png'} alt="" />
+                    </Profile>
+                </aside>
+            </Content>
+        </Container>
+    );
 }
