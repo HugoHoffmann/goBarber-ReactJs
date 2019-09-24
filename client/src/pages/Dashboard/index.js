70f339f2f381735976d0293322522819d999aadf
Hugo <hugohoffmann04@gmail.com>
2019-09-24 14:01:50 -0300

    chore: add style in dashboard

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,9 +1,34 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import api from '~/services/api';
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md'
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
   return (
-    <div />
+    <Container>
+      <header>
+        <button type="button" >
+          <MdChevronLeft size={36} color="#fff"/>
+        </button>
+        <strong>31 de maio</strong>
+        <button type="button" >
+          <MdChevronRight size={36} color="#fff"/>
+        </button>
+      </header>
+      <ul>
+        <Time past>
+          <strong>10</strong>
+          <span>Teste</span>
+        </Time>
+        <Time available>
+          <strong>10</strong>
+          <span>Teste</span>
+        </Time>
+        <Time>
+          <strong>10</strong>
+          <span>Teste</span>
+        </Time>
+      </ul>
+    </Container>
   );
 }
